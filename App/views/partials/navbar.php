d558e825e29a34248223874f59f710888a4fae38
habaArakha <hebatullah.ammar.rakha@gmail.com>
2024-04-02 06:32:08 +0200

    fixed : login issue

--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -7,23 +7,48 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="/" class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/') ? 'active' : ''; ?>">Home</a>
-            <a href="/testimonial" class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/testimonial') ? 'active' : ''; ?>">Testimonial</a>
-            <a href="/about" class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/about') ? 'active' : ''; ?>">About</a>
-            <a href="/apply" class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/apply') ? 'active' : ''; ?>">Apply Job</a>
+            <a href="/"
+                class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/') ? 'active' : ''; ?>">Home</a>
+            <a href="/testimonial"
+                class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/testimonial') ? 'active' : ''; ?>">Testimonial</a>
+            <a href="/about"
+                class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/about') ? 'active' : ''; ?>">About</a>
+            <a href="/apply"
+                class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/apply') ? 'active' : ''; ?>">Apply
+                Job</a>
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle <?php echo ($_SERVER['REQUEST_URI'] == '/jobs' || $_SERVER['REQUEST_URI'] == '/category') ? 'active' : ''; ?>" data-bs-toggle="dropdown">Jobs</a>
+                <a href="#"
+                    class="nav-link dropdown-toggle <?php echo ($_SERVER['REQUEST_URI'] == '/jobs' || $_SERVER['REQUEST_URI'] == '/category') ? 'active' : ''; ?>"
+                    data-bs-toggle="dropdown">Jobs</a>
                 <div class="dropdown-menu rounded-0 m-0">
-                    <a href="/jobs" class="dropdown-item <?php echo ($_SERVER['REQUEST_URI'] == '/jobs') ? 'active' : ''; ?>">Job List</a>
-                    <a href="/category" class="dropdown-item <?php echo ($_SERVER['REQUEST_URI'] == '/category') ? 'active' : ''; ?>">Job Category</a>
+                    <a href="/jobs"
+                        class="dropdown-item <?php echo ($_SERVER['REQUEST_URI'] == '/jobs') ? 'active' : ''; ?>">Job
+                        List</a>
+                    <a href="/category"
+                        class="dropdown-item <?php echo ($_SERVER['REQUEST_URI'] == '/category') ? 'active' : ''; ?>">Job
+                        Category</a>
                 </div>
             </div>>
-            <a href="/contact" class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/contact') ? 'active' : ''; ?>">Contact</a>
-            <a href="/signup-type" class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/signup-type') ? 'active' : ''; ?>">Sign Up</a>
-            <a href="/contact" class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/login') ? 'active' : ''; ?>">Log in</a>
-            <a href="/contact" class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/logout') ? 'active' : ''; ?>">Logout</a>
+            <a href="/contact"
+                class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/contact') ? 'active' : ''; ?>">Contact</a>
+
+
         </div>
-        <a href="/jobs/create" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Post A Job<i
-                class="fa fa-arrow-right ms-3"></i></a>
+        <?php if (!isset($_SESSION['token'])): ?>
+            <a href="/signup-type"
+                class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/signup-type') ? 'active' : ''; ?>">Sign
+                Up</a>
+            <a href="/contact"
+                class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/login') ? 'active' : ''; ?>">Log in</a>
+        <?php elseif (isset($_SESSION['token']) && $_SESSION['account'] == 'organization'): ?>
+            <a href="/contact"
+                class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/logout') ? 'active' : ''; ?>">Logout</a>
+            <a href="/jobs/create" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Post A Job<i
+                    class="fa fa-arrow-right ms-3"></i></a>
+        <?php else: ?>
+            <a href="/contact"
+                class="nav-item nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/logout') ? 'active' : ''; ?>">Logout</a>
+        <?php endif ?>
+    </div>
     </div>
 </nav>
